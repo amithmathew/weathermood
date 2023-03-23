902101b218b30437b2108798bd0fd3702a314b54
Amith Mathew <git@amithm.ca>
2023-03-22 23:17:38 -0400

    build

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 fetch('gen/prompt.txt')
   .then(response => response.text())
   .then(data => {
-    const footerText = document.getElementById('prompt');
+    const footerText = document.getElementById('prompt-text');
     footerText.innerText = data;
   });
   
